fbce0d95586ac0e5094fe0d74e790fe9ae62da5c
andre-alck <andrealck1@gmail.com>
2022-10-15 00:30:13 -0300

    test: grouping tests

--- test/user/models/user_test.dart
+++ test/user/models/user_test.dart
@@ -2,44 +2,52 @@ import 'package:test/test.dart';
 import 'package:user_serializable/user/models/user.dart';
 
 void main() {
-  test(
-    'Given information When user is created Then compare to Input',
+  const String name = 'name';
+  const int age = 99;
+
+  group(
+    'User',
     () {
-      final user = User(
-        'name',
-        99,
+      test(
+        'Given information When user is created Then compare to Input',
+        () {
+          final user = User(
+            name,
+            age,
+          );
+
+          expect(
+            user.name,
+            'name',
+          );
+          expect(
+            user.age,
+            99,
+          );
+        },
       );
 
-      expect(
-        user.name,
-        'name',
-      );
-      expect(
-        user.age,
-        99,
-      );
-    },
-  );
+      test(
+        'Given JSON When user is created Then compare data between them',
+        () {
+          Map<String, dynamic> json = {
+            "name": name,
+            "age": age,
+          };
 
-  test(
-    'Given JSON When user is created Then compare data between them',
-    () {
-      Map<String, dynamic> json = {
-        "name": 'name',
-        "age": 99,
-      };
+          final user = User.fromJson(
+            json,
+          );
 
-      final user = User.fromJson(
-        json,
-      );
-
-      expect(
-        user.name,
-        'name',
-      );
-      expect(
-        user.age,
-        99,
+          expect(
+            user.name,
+            'name',
+          );
+          expect(
+            user.age,
+            99,
+          );
+        },
       );
     },
   );
