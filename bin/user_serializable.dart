027c373352f6839fb857e7ee62d623eaec957cc2
andre-alck <andrealck1@gmail.com>
2022-10-14 23:55:46 -0300

    refactor: delete unsused files

--- bin/user_serializable.dart
+++
@@ -1,5 +0,0 @@
-import 'package:user_serializable/user_serializable.dart' as user_serializable;
-
-void main(List<String> arguments) {
-  print('Hello world: ${user_serializable.calculate()}!');
-}
--- test/user_serializable_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:user_serializable/user_serializable.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
