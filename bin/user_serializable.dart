db425d83444319eab5327c4c9dd79b21905eb5b9
andre-alck <andrealck1@gmail.com>
2022-10-14 23:37:55 -0300

    init commit

---
+++ bin/user_serializable.dart
@@ -0,0 +1,5 @@
+import 'package:user_serializable/user_serializable.dart' as user_serializable;
+
+void main(List<String> arguments) {
+  print('Hello world: ${user_serializable.calculate()}!');
+}
---
+++ test/user_serializable_test.dart
@@ -0,0 +1,8 @@
+import 'package:user_serializable/user_serializable.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
