bd6e6b32f57c64860292b357a7b6ca46c38d1c59
andre-alck <andrealck1@gmail.com>
2022-10-19 12:28:00 -0300

    test: user + shoes

--- test/user/models/user_test.dart
+++ test/user/models/user_test.dart
@@ -5,43 +5,20 @@ import 'package:user_serializable/user/models/user.dart';
 
 void main() {
   group(
-    'User',
+    'All tests',
     () {
-      test(
-        'Given required data When user is created Then check user',
-        () {
-          const String name = 'name';
-          const int age = 99;
-
-          final user = User(
-            name: name,
-            age: age,
-          );
-
-          expect(
-            user.name,
-            'name',
-          );
-          expect(
-            user.age,
-            99,
-          );
-        },
-      );
-
       group(
-        'JSON',
+        'User',
         () {
           test(
-            'Given required data as map When user is created by fromJson Then check user',
+            'Given required data When user is created Then check user',
             () {
-              Map<String, dynamic> json = {
-                "name": "name",
-                "age": 99,
-              };
+              const String name = 'name';
+              const int age = 99;
 
-              final user = User.fromJson(
-                json,
+              final user = User(
+                name: name,
+                age: age,
               );
 
               expect(
@@ -55,26 +32,103 @@ void main() {
             },
           );
 
-          test(
-            'Given overcomplete data as map When user is created by fromJson Then check user',
+          group(
+            'JSON',
             () {
-              Map<String, dynamic> json = {
-                "name": "name",
-                "age": 99,
-                "foo": "bar",
-              };
+              test(
+                'Given required data as map When user is created by fromJson Then check user',
+                () {
+                  Map<String, dynamic> json = {
+                    "name": "name",
+                    "age": 99,
+                  };
 
-              final user = User.fromJson(
-                json,
+                  final user = User.fromJson(
+                    json,
+                  );
+
+                  expect(
+                    user.name,
+                    'name',
+                  );
+                  expect(
+                    user.age,
+                    99,
+                  );
+                },
               );
 
-              expect(
-                user.name,
-                'name',
+              test(
+                'Given overcomplete data as map When user is created by fromJson Then check user',
+                () {
+                  Map<String, dynamic> json = {
+                    "name": "name",
+                    "age": 99,
+                    "foo": "bar",
+                  };
+
+                  final user = User.fromJson(
+                    json,
+                  );
+
+                  expect(
+                    user.name,
+                    'name',
+                  );
+                  expect(
+                    user.age,
+                    99,
+                  );
+                },
               );
-              expect(
-                user.age,
-                99,
+            },
+          );
+
+          group(
+            'String',
+            () {
+              test(
+                'Given required data as String When user is created by fromJson Then check user',
+                () {
+                  String data = '{"name":"name", "age":99}';
+
+                  final user = User.fromJson(
+                    json.decode(
+                      data,
+                    ),
+                  );
+
+                  expect(
+                    user.name,
+                    'name',
+                  );
+                  expect(
+                    user.age,
+                    99,
+                  );
+                },
+              );
+
+              test(
+                'Given overcomplete data as String When user is created by fromJson Then check user',
+                () {
+                  String data = '{"name":"name", "age":99, "foo":"bar"}';
+
+                  final user = User.fromJson(
+                    json.decode(
+                      data,
+                    ),
+                  );
+
+                  expect(
+                    user.name,
+                    'name',
+                  );
+                  expect(
+                    user.age,
+                    99,
+                  );
+                },
               );
             },
           );
@@ -82,12 +136,52 @@ void main() {
       );
 
       group(
-        'String',
+        'User and Shoes',
         () {
           test(
-            'Given required data as String When user is created by fromJson Then check user',
+            'Given required data (including shoes) as map When user is created by fromJson Then check user',
             () {
-              String data = '{"name":"name", "age":99}';
+              Map<String, dynamic> json = {
+                "name": "name",
+                "age": 99,
+                "shoes": {
+                  "color": "blue",
+                  "size": 40,
+                  "price": 100.0,
+                },
+              };
+
+              final user = User.fromJson(
+                json,
+              );
+              expect(
+                user.name,
+                "name",
+              );
+              expect(
+                user.age,
+                99,
+              );
+              expect(
+                user.shoes?.color,
+                "blue",
+              );
+              expect(
+                user.shoes?.size,
+                40,
+              );
+              expect(
+                user.shoes?.price,
+                100.0,
+              );
+            },
+          );
+
+          test(
+            'Given required data (including shoes) as map When user is created by fromJson Then check user',
+            () {
+              String data =
+                  '{"name": "name","age": 99,"shoes": {"color": "blue","size": 40,"price": 100.0}}';
 
               final user = User.fromJson(
                 json.decode(
@@ -97,33 +191,23 @@ void main() {
 
               expect(
                 user.name,
-                'name',
+                "name",
               );
               expect(
                 user.age,
                 99,
               );
-            },
-          );
-
-          test(
-            'Given overcomplete data as String When user is created by fromJson Then check user',
-            () {
-              String data = '{"name":"name", "age":99, "foo":"bar"}';
-
-              final user = User.fromJson(
-                json.decode(
-                  data,
-                ),
-              );
-
               expect(
-                user.name,
-                'name',
+                user.shoes?.color,
+                "blue",
               );
               expect(
-                user.age,
-                99,
+                user.shoes?.size,
+                40,
+              );
+              expect(
+                user.shoes?.price,
+                100.0,
               );
             },
           );
